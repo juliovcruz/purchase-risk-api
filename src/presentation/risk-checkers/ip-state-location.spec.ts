762667db68e8868e50df5450d20353bb6d6252c4
Julio V. Cruz <juliovcruz0@gmail.com>
2020-07-29 03:07:55 -0300

    test: ensure IpStateLocationChecker return 0 if ipLocation and state is equal

---
+++ src/presentation/risk-checkers/ip-state-location.spec.ts
@@ -0,0 +1,37 @@
+import { IpStateLocationChecker } from './ip-state-location'
+import { TransactionModel } from '../../domain/models/transaction'
+import { CustomerModel } from '../../domain/models/customer'
+
+const makeFakeCustomer = (): CustomerModel => {
+  return {
+    id: 'any_id',
+    name: 'any_name',
+    birth_date: 'any_birth_date',
+    state: 'any_location',
+    phone: 'any_phone'
+  }
+}
+
+const makeFakeTransaction = (): TransactionModel => {
+  return {
+    id: 'any_id',
+    value: 10,
+    paid_at: 'any_date',
+    ip_location: 'any_location',
+    card_hold_name: 'any_card_hold_name',
+    customer: makeFakeCustomer()
+  }
+}
+
+const makeSut = (): IpStateLocationChecker => {
+  return new IpStateLocationChecker()
+}
+
+describe('IpStateLocation Checker', () => {
+  test('Should return 0 if ipLocation and state is equal', () => {
+    const sut = makeSut()
+    const transaction = makeFakeTransaction()
+    const result = sut.verifyRisk(transaction)
+    expect(result).toBe(0)
+  })
+})
