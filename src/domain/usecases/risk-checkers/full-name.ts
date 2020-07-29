5b7888bc9949fad8177c4680e541a8378226ca19
Julio V. Cruz <juliovcruz0@gmail.com>
2020-07-29 16:42:38 -0300

    feat: ensure FullNameChecker return levelRisk 3 if only first name is provided

--- src/domain/usecases/risk-checkers/full-name.ts
+++ src/domain/usecases/risk-checkers/full-name.ts
@@ -8,7 +8,20 @@ export class FullNameChecker implements RiskChecker {
     this.levelRisk = levelRisk
   }
 
+  verifySecondNameEmpty (str: string): boolean {
+    if (str === undefined) return true
+    if (str.length < 2) return true
+    return false
+  }
+
   verifyRisk (transaction: TransactionModel): number {
+    const secondNameCard = transaction.card_hold_name.split(' ')[1]
+    const secondNameCustomer = transaction.customer.name.split(' ')[1]
+    if (this.verifySecondNameEmpty(secondNameCard)) return this.levelRisk[3]
+    if (this.verifySecondNameEmpty(secondNameCustomer)) return this.levelRisk[3]
+    // if (secondNameCard === undefined) return this.levelRisk[3]
+    // if (secondNameCustomer === undefined) return this.levelRisk[3]
+    console.log(secondNameCard, secondNameCustomer)
     return 0
   }
 }
