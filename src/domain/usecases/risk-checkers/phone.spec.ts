b58a8134f885ff90033e96c9edaf17d3f62a20e7
Julio V. Cruz <juliovcruz0@gmail.com>
2020-07-29 13:26:37 -0300

    test: ensure PhoneChecker return levelRisk 0 if phone DDD is compatible with all locations

---
+++ src/domain/usecases/risk-checkers/phone.spec.ts
@@ -0,0 +1,38 @@
+import { PhoneChecker } from './phone'
+import { TransactionModel } from '../../models/transaction'
+import { CustomerModel } from '../../models/customer'
+
+const makeFakeCustomer = (): CustomerModel => {
+  return {
+    id: 'any_id',
+    name: 'any_name',
+    birth_date: 'any_birth_date',
+    state: 'RJ/BR',
+    phone: 'any_phone'
+  }
+}
+
+const makeFakeTransaction = (): TransactionModel => {
+  return {
+    id: 'any_id',
+    value: 10,
+    paid_at: 'any_date',
+    ip_location: 'RJ/BR',
+    card_hold_name: 'any_card_hold_name',
+    customer: makeFakeCustomer()
+  }
+}
+
+const makeSut = (): PhoneChecker => {
+  const levelRisk = [0, 1, 2, 3, 4, 5]
+  return new PhoneChecker(levelRisk)
+}
+
+describe('Phone Checker', () => {
+  test('Should return levelRisk 0 if phone DDD is compatible with all locations', () => {
+    const sut = makeSut()
+    const transaction = makeFakeTransaction()
+    const result = sut.verifyRisk(transaction)
+    expect(result).toBe(sut.levelRisk[0])
+  })
+})
