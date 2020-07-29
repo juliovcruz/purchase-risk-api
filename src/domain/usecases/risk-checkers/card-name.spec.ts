a6194e6da7a30eec9aa9675bfb16a6e9e6998cbe
Julio V. Cruz <juliovcruz0@gmail.com>
2020-07-29 16:56:09 -0300

    test: ensure CardNameChecker return levelRisk 0 if card name and customer name is equal

---
+++ src/domain/usecases/risk-checkers/card-name.spec.ts
@@ -0,0 +1,40 @@
+import { CustomerModel } from '../../models/customer'
+import { TransactionModel } from '../../models/transaction'
+import { CardNameChecker } from './card-name'
+
+const makeFakeCustomer = (): CustomerModel => {
+  return {
+    id: 'any_id',
+    name: 'any_name',
+    birth_date: 'any_birth_date',
+    state: 'RJ/BR',
+    phone: '24 99999-9999'
+  }
+}
+
+const makeFakeTransaction = (): TransactionModel => {
+  return {
+    id: 'any_id',
+    value: 10,
+    paid_at: 'any_date',
+    ip_location: 'RJ/BR',
+    card_hold_name: 'any_card_hold_name',
+    customer: makeFakeCustomer()
+  }
+}
+
+const makeSut = (): CardNameChecker => {
+  const levelRisk = [0, 1, 2, 3, 4]
+  return new CardNameChecker(levelRisk)
+}
+
+describe('CardName Checker', () => {
+  test('Should return levelRisk 0 if card name and customer name is equal', () => {
+    const sut = makeSut()
+    const transaction = makeFakeTransaction()
+    transaction.card_hold_name = 'any_name'
+    transaction.customer.name = 'any_name'
+    const result = sut.verifyRisk(transaction)
+    expect(result).toBe(sut.levelRisk[0])
+  })
+})
