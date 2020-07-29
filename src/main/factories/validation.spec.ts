2d4e8a329d4e3e34c3993a1df071967c0fa1770f
Julio V. Cruz <juliovcruz0@gmail.com>
2020-07-29 10:32:50 -0300

    test: ensure call ValidationComposite factory with all validations

---
+++ src/main/factories/validation.spec.ts
@@ -0,0 +1,18 @@
+import { makeValidation } from './validation'
+import { Validation } from '../../presentation/protocols/validation'
+import { RequiredFieldValidation } from '../../presentation/validators/required-field-validation'
+import { ValidationComposite } from '../../presentation/validators/validation-composite'
+
+jest.mock('../../presentation/validators/validation-composite')
+
+describe('Validation Factory', () => {
+  test('Should call ValidationComposite with all validations', () => {
+    makeValidation()
+    const validations: Validation[] = []
+    const fields = ['id', 'name']
+    for (const field of fields) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
