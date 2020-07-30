3f0ca10dbb23dd75730945c83ca5c115b4febaeb
Julio V. Cruz <juliovcruz0@gmail.com>
2020-07-30 17:32:01 -0300

    feat: ensure TransactionRiskController call verifyRisk with all transactions

--- src/presentation/controllers/transaction-risk.ts
+++ src/presentation/controllers/transaction-risk.ts
@@ -3,6 +3,7 @@ import { HttpRequest, HttpResponse } from '../protocols/http'
 import { RiskChecker } from '../../domain/usecases/protocols/risk-checker'
 import { Validation } from '../protocols/validation'
 import { successRisk } from '../helpers/http-helpers'
+import { RiskBody } from '../../domain/usecases/protocols/http-risk'
 
 export class TransactionRiskController implements Controller {
   private readonly riskChecker: RiskChecker
@@ -13,16 +14,15 @@ export class TransactionRiskController implements Controller {
     this.validation = validation
   }
 
-  handle (httpRequest: HttpRequest): HttpResponse {
-    const response = [
-      {
-        id: httpRequest.body[0].id,
-        score: 0
-      },
-      {
-        id: httpRequest.body[1].id,
-        score: 0
-      }]
+  handle (transactions: HttpRequest): HttpResponse {
+    const response: RiskBody[] = []
+    for (const transaction of transactions.body) {
+      const scoreR = this.riskChecker.verifyRisk(transaction)
+      response.push({
+        id: transaction.id,
+        score: scoreR
+      })
+    }
     return successRisk(response)
   }
 }
