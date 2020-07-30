b960acc16827b0d96aa8b5b320b4bc286b413650
Julio V. Cruz <juliovcruz0@gmail.com>
2020-07-30 17:11:42 -0300

    fix: add more one levelRisk in RiskChecker factory test

--- src/main/factories/risk-checker.spec.ts
+++ src/main/factories/risk-checker.spec.ts
@@ -8,10 +8,10 @@ describe('RiskChecker Composite', () => {
   test('Should call RiskCheckerComposite with all checkers', () => {
     makeRiskChecker()
     const checkers: RiskChecker[] = []
-    checkers.push(new CardNameChecker([0, 1, 2, 3, 4]))
-    checkers.push(new PhoneDDDChecker([0, 1, 2, 3, 4]))
-    checkers.push(new FullNameChecker([0, 1, 2, 3, 4]))
-    checkers.push(new LocationChecker([0, 1, 2, 3, 4]))
+    checkers.push(new CardNameChecker([0, 1, 2, 3, 4, 5]))
+    checkers.push(new PhoneDDDChecker([0, 1, 2, 3, 4, 5]))
+    checkers.push(new FullNameChecker([0, 1, 2, 3, 4, 5]))
+    checkers.push(new LocationChecker([0, 1, 2, 3, 4, 5]))
 
     expect(RiskCheckerComposite).toHaveBeenCalledWith(checkers)
   })
