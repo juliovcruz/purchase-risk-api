e4eaab8154d978931370ab2b7e42b0f72e7337fd
Julio V. Cruz <juliovcruz0@gmail.com>
2020-07-30 16:56:44 -0300

    fix: error in setup middlewares

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import { bodyParser, contentType, cors } from './middlewares'
+import transactionRoute from '../routes/transaction-risk'
 
 const app = express()
 app.use(bodyParser)
 app.use(contentType)
 app.use(cors)
+transactionRoute(app)
 export default app
