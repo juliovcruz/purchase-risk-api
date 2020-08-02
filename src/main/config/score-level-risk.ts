8d881778348430d3390bb67ba22f4c344849ce81
Julio V. Cruz <juliovcruz0@gmail.com>
2020-08-02 15:11:25 -0300

    refactor: move levelRisk scores to a config file

---
+++ src/main/config/score-level-risk.ts
@@ -0,0 +1,7 @@
+export default {
+  cardNameRisk: [0, 5, 15, 20, 60, 85],
+  phoneDDDRisk: [0, 5, 20, 30, 45, 60],
+  fullNameRisk: [0, 5, 15, 16, 60, 85],
+  locationRisk: [0, 13, 15, 20, 55, 55],
+  validPhoneRisk: [0, 13, 15, 20, 55, 55]
+}
