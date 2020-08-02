f00a899fd675b7d88b825d49e414502858893c54
Julio V. Cruz <juliovcruz0@gmail.com>
2020-08-01 23:33:23 -0300

    chore: change dependencies

--- src/main/main.ts
+++ src/main/main.ts
@@ -3,4 +3,5 @@ const PORT = 5000
 
 app.listen(PORT, () => {
   console.log(`Server is running in http://localhost:${PORT}`)
+  console.log(`For use the api, send POST in http://localhost:${PORT}/risk`)
 })
